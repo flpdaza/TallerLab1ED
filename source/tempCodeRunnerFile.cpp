57678a73560367c3f8155b1b6b61927ce97d779c
flpdaza <felipe.daza@alumnos.ucn.cl>
2023-10-09 22:22:57 -0300

    actualizando

--- source/tempCodeRunnerFile.cpp
+++
@@ -1,6 +0,0 @@
-    cout<<"1.- Mostrar todos los Software"<<endl;
-        cout<<"2.- Agregar amigo"<<endl;
-        cout<<"3.- Agregar archivos en Software (Ofimatica)"<<endl;
-        cout<<"4.- Salir"<<endl;
-        cout<<"Ingrese opcion: ";
-        cin>> opcion;
